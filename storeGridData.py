7a8897a8f0cd80325cf700dfc2965737cff2f247
alexfatalix <alexfatalix@gmail.com>
2020-10-22 00:19:44 +0300

    Remove bug on display With bug on double click

--- storeGridData.py
+++ storeGridData.py
@@ -1,10 +1,12 @@
 import pygame as pg
+from drawXO import DrawXO
 
+drawXO = DrawXO()
 
 class StoreGridData:
 
     def __init__(self):
-        self. defaultGrid = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.defaultGrid = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.defaultMidPoints = ((100, 100),
                                  (300, 100),
                                  (500, 100),
@@ -15,12 +17,22 @@ class StoreGridData:
                                  (300, 500),
                                  (500, 500))
 
-    def storeXO(self, XO, midPos):
+
+    def storeXO(self, XO, midPos, surface, thickness, radius, BLUE = (0, 0, 255), RED = (255, 0, 0)):
         numberRect = self.countNumberOfRect(midPos)
-        if XO == False:
-            self.defaultGrid[numberRect - 1] = 1
-        else:
-            self.defaultGrid[numberRect - 1] = 2
+        if self.defaultGrid[numberRect - 1] == 0:
+            if not XO:
+                self.defaultGrid[numberRect - 1] = 1
+                print(self.defaultGrid)
+            else:
+                self.defaultGrid[numberRect - 1] = 2
+                print(self.defaultGrid)
+        for i in range(0, 9):
+            if self.defaultGrid[i] == 1:
+                drawXO.drawCross(surface, BLUE, self.defaultMidPoints[i], thickness + 10)
+            elif self.defaultGrid[i] == 2:
+                drawXO.drawCircle(surface, RED, self.defaultMidPoints[i], radius, thickness)
+
 
     def countNumberOfRect(self, midPos):
         counterRect = 0
@@ -32,4 +44,3 @@ class StoreGridData:
                 if rect.collidepoint(midPos):
                     temp = counterRect
         return temp
-
